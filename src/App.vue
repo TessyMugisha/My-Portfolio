1a1b1d780889011b10972ba4221021a1e7b99169
pmtes <pmtessy17@gmail.com>
2025-09-02 01:11:22 -0500

    Point app to components/Home.vue and add full page layout

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
-<script setup>
-  import Home from "./views/Home.vue";
-  import { useRouter } from "vue-router";
+// src/router/index.ts (or .js)
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/components/Home.vue'   // <-- changed path
 
-  const router = useRouter();
-</script>
+export default createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: Home },
+    // other routes...
+  ],
+})
 
-<template>
-  <div class="mx-auto my-6 max-w-xs sm:max-w-sm md:max-w-lg">
-    <div class="flex flex-col">
-      <div class="p-15">
-        <div class="basis-full text-right text-[30pt] font-bold tracking-wide">
-          <RouterLink to="/">TM<span class="text-orange"></span></RouterLink>
-        </div>
-      </div>
-      <div class="h-[7rem] md:h-[5rem]"></div>
-
-      <RouterView />
-    </div>
-  </div>
-</template>
